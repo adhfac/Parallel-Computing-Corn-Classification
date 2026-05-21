401f58f60aab020681683e1c60a42b9da86fe597
adhfac <mdhafaadjiesaputra_2327250081@mhs.mdp.ac.id>
2026-05-21 20:24:25 +0700

    feat: added graphs for visualization

---
+++ create_graph.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df=pd.read_csv(
+    'benchmark_result.csv'
+)
+
+# ======================
+# EXECUTION TIME
+# ======================
+
+plt.figure(figsize=(8,5))
+
+plt.plot(
+    df['dataset'],
+    df['serial_time'],
+    marker='o',
+    label='Serial'
+)
+
+plt.plot(
+    df['dataset'],
+    df['parallel_time'],
+    marker='o',
+    label='Parallel'
+)
+
+plt.xlabel('Dataset Size')
+plt.ylabel('Seconds')
+
+plt.title(
+    'Execution Time Comparison'
+)
+
+plt.legend()
+plt.grid()
+
+plt.savefig(
+    'execution_time.png'
+)
+
+plt.close()
+
+# ======================
+# SPEEDUP
+# ======================
+
+plt.figure(figsize=(8,5))
+
+plt.bar(
+    df['dataset'],
+    df['speedup']
+)
+
+plt.xlabel('Dataset Size')
+
+plt.ylabel('Speedup')
+
+plt.title(
+    'Speedup Analysis'
+)
+
+plt.grid(axis='y')
+
+plt.savefig(
+    'speedup.png'
+)
+
+plt.close()
+
+# ======================
+# THROUGHPUT
+# ======================
+
+x=np.arange(len(df))
+
+width=0.35
+
+plt.figure(figsize=(9,5))
+
+plt.bar(
+    x-width/2,
+    df['serial_throughput(img/s)'],
+    width,
+    label='Serial'
+)
+
+plt.bar(
+    x+width/2,
+    df['parallel_throughput(img/s)'],
+    width,
+    label='Parallel'
+)
+
+plt.xticks(
+    x,
+    df['dataset']
+)
+
+plt.xlabel(
+    'Dataset Size'
+)
+
+plt.ylabel(
+    'Images / Second'
+)
+
+plt.title(
+    'Throughput Comparison'
+)
+
+plt.legend()
+
+plt.savefig(
+    'throughput.png'
+)
+
+plt.close()
+
+print(
+    'Visualization completed.'
+)
